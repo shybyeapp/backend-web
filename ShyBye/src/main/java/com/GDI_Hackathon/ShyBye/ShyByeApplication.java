f0e1d2c31ce6c14bba31a0603dc111c1bd7c987b
Anthony <anthony.l.ahrens@gmail.com>
2022-11-16 15:00:01 -0600

    Java Spring IO Intializer Setup

---
+++ ShyBye/src/main/java/com/GDI_Hackathon/ShyBye/ShyByeApplication.java
@@ -0,0 +1,13 @@
+package com.GDI_Hackathon.ShyBye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyByeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShyByeApplication.class, args);
+	}
+
+}
